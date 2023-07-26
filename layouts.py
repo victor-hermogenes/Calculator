44cfdba062e5330628ff3ab097e02100455a5780
victor-hermogenes <victorltrgh@gmail.com>
2023-07-26 09:24:47 -0300

    translated to english

--- layouts.py
+++ layouts.py
@@ -22,6 +22,7 @@ def mkup_layout():
         [Sg.Text('Equations:')],
         [Sg.Output(key='calculo', size=(40, 4))],
         [Sg.Button('Run'), Sg.CloseButton('Back', button_color=('black', 'lightgray'))],
+        [Sg.Text('Created by Victor G. Hermogenes.')]
     ]
 
     # Window
@@ -74,18 +75,19 @@ def checar_lqd():
 
     # Layout
     layout_lqd = [
-        [Sg.Text('Essa calculadora vai te ajudar a descobrir o quanto \nsobrou da sua venda')],
-        [Sg.Text('Preencha os dados abaixo')],
-        [Sg.Text('Preço bruto da venda:', size=(20, 1)), Sg.Input('0', key='bruto', size=(8, 1))],
-        [Sg.Text('Percentual de comissão:', size=(20, 1)), Sg.Input('0', key='comissao', size=(8, 1))],
-        [Sg.Text('Custo do produto:', size=(20, 1)), Sg.Input('0', key='custo', size=(8, 11))],
-        [Sg.Text('Taxa fixa:', size=(20, 1)), Sg.Input('0', key='txfixa', size=(8, 11))],
-        [Sg.Text('Frete pago pelo cliente:', size=(20, 1)), Sg.Input('0', key='frtcliente', size=(8, 11))],
-        [Sg.Text('Frete pago por você:', size=(20, 1)), Sg.Input('0', key='frtvc', size=(8, 11))],
-        [Sg.Text('OBS: CASO NÃO TENHA TAXA OU FRETE, IGNORAR')],
-        [Sg.Text('Etapas do cálculo:')],
+        [Sg.Text('This calculator will help you figure out how much is \nleft over from your sale')],
+        [Sg.Text('Fill in the details below')],
+        [Sg.Text('Gross sale price:', size=(20, 1)), Sg.Input('0', key='bruto', size=(8, 1))],
+        [Sg.Text('Commission percentage:', size=(20, 1)), Sg.Input('0', key='comissao', size=(8, 1))],
+        [Sg.Text('Product cost:', size=(20, 1)), Sg.Input('0', key='custo', size=(8, 11))],
+        [Sg.Text('Taxes', size=(20, 1)), Sg.Input('0', key='txfixa', size=(8, 11))],
+        [Sg.Text('Shipping paid by customer:', size=(20, 1)), Sg.Input('0', key='frtcliente', size=(8, 11))],
+        [Sg.Text('Shipping paid by you:', size=(20, 1)), Sg.Input('0', key='frtvc', size=(8, 11))],
+        [Sg.Text('NOTE: IF THERE IS NO FEE OR SHIPPING, IGNORE')],
+        [Sg.Text('Calculation steps:')],
         [Sg.Output(key='calculo', size=(45, 6))],
-        [Sg.Button('Calcular'), Sg.CloseButton('Voltar', button_color=('black', 'lightgray'))]
+        [Sg.Button('Run'), Sg.CloseButton('Back', button_color=('black', 'lightgray'))],
+        [Sg.Text('Created by Victor G. Hermogenes.')]
     ]
 
     # Window
@@ -109,15 +111,15 @@ def checar_lqd():
                 frtcliente = round(float(values_lqd['frtcliente'].replace(',', '.')), 2)
                 frtvc = round(float(values_lqd['frtvc'].replace(',', '.')), 2)
                 custo = round(float(values_lqd['custo'].replace(',', '.')), 2)
-                calculo = f"""etapa1 = {bruto}-({bruto}*{comissao})-{txfixa}+({frtcliente}-{frtvc})
-etapa2 = {bruto}-{round(bruto*comissao, 2)}-{txfixa}+{(frtcliente-frtvc)}
-etapa3 = {round(bruto-(bruto*comissao), 2)}-{round(txfixa+(frtcliente-frtvc), 2)}
-resultado = R$ {round(bruto-(bruto*comissao)-txfixa+(frtcliente-frtvc), 2)}
-percentual = {round(((bruto-(bruto*comissao)-txfixa+(frtcliente-frtvc))/custo-1)*100, 2)}%""".replace('.', ',')
+                calculo = f"""step1 = {bruto}-({bruto}*{comissao})-{txfixa}+({frtcliente}-{frtvc})
+                step2 = {bruto}-{round(bruto*comissao, 2)}-{txfixa}+{(frtcliente-frtvc)}
+                step3 = {round(bruto-(bruto*comissao), 2)}-{round(txfixa+(frtcliente-frtvc), 2)}
+                result = R$ {round(bruto-(bruto*comissao)-txfixa+(frtcliente-frtvc), 2)}
+                percentage = {round(((bruto-(bruto*comissao)-txfixa+(frtcliente-frtvc))/custo-1)*100, 2)}%""".replace('.', ',')
                 window_lqd['calculo'].update(calculo)
 
             except ValueError:
-                Sg.Popup('Impossível calcular, algum dos campos está vazio! Tente novamente', location=(350, 0))
+                Sg.Popup('Impossible to calculate, one of the fields is empty! Try again', location=(350, 0))
                 break
 
     window_lqd.close()
@@ -128,15 +130,16 @@ def descobrir_promocao():
     Sg.theme('DarkBlack')
     # Layout
     layout_pmc = [
-        [Sg.Text("""Esta calculadora serve para descobrir o quanto do seu preço você precisa aumentar para
-ofertar a promoção que você pretende.""")],
-        [Sg.Text('Preço normal:', size=(20, 1)), Sg.Input('0', key='normal', size=(8, 1))],
-        [Sg.Text('Margem de promoção:', size=(20, 1)), Sg.Input('0', key='margem', size=(8, 1))],
-        [Sg.Text('Frete:', size=(20, 1)), Sg.Input('0', key='frete', size=(8, 1))],
-        [Sg.Text('OBS: CASO NÃO TENHA FRETE, IGNORAR')],
-        [Sg.Text('Etapas do cálculo:')],
+        [Sg.Text("""This calculator is for figuring out how much of your price you need to increase to
+                    offer the promotion you want.""")],
+        [Sg.Text('Regular price:', size=(20, 1)), Sg.Input('0', key='normal', size=(8, 1))],
+        [Sg.Text('Promotion Margin:', size=(20, 1)), Sg.Input('0', key='margem', size=(8, 1))],
+        [Sg.Text('Freight:', size=(20, 1)), Sg.Input('0', key='frete', size=(8, 1))],
+        [Sg.Text('NOTE: IF THERE IS NO SHIPPING, IGNORE')],
+        [Sg.Text('Calculation steps:')],
         [Sg.Output(key='calculo', size=(45, 5))],
-        [Sg.Button('Calcular'), Sg.Button('Voltar', button_color=('black', 'lightgray'))]
+        [Sg.Button('Run'), Sg.Button('Back', button_color=('black', 'lightgray'))],
+        [Sg.Text('Created by Victor G. Hermogenes.')]
     ]
 
     # Window
@@ -147,22 +150,22 @@ ofertar a promoção que você pretende.""")],
         event_pmc, values_pmc = window_pmc.read()
 
         # Fechar caso necessário
-        if event_pmc == 'Voltar' or event_pmc == Sg.WIN_CLOSED:
+        if event_pmc == 'Back' or event_pmc == Sg.WIN_CLOSED:
             break
 
         # Seguir com os cálculos
-        elif event_pmc == 'Calcular':
+        elif event_pmc == 'Run':
             try:
                 normal = round(float(values_pmc['normal'].replace(',', '.')), 2)
                 margem = round(float(values_pmc['margem'].replace(',', '.').replace('%', '')), 2)
                 frete = round(float(values_pmc['frete'].replace(',', '.')), 2)
-                calculo = f"""etapa1 = ({normal}+{frete})/(1-({margem}/100))
-etapa2 = {normal+frete}/(1-{margem/100})
-etapa3 = {normal+frete}/{(1-(margem/100))}
-resultado = R$ {round((normal+frete)/(1-(margem/100)), 2)}""".replace('.', ',')
+                calculo = f"""step1 = ({normal}+{frete})/(1-({margem}/100))
+                step2 = {normal+frete}/(1-{margem/100})
+                step3 = {normal+frete}/{(1-(margem/100))}
+                result = R$ {round((normal+frete)/(1-(margem/100)), 2)}""".replace('.', ',')
                 window_pmc['calculo'].update(calculo)
             except ValueError:
-                Sg.Popup('Impossível calcular, algum dos campos está vazio! Tente novamente', location=(350, 0))
+                Sg.Popup('Impossible to calculate, one of the fields is empty! Try again', location=(350, 0))
                 break
 
     window_pmc.close()
@@ -174,36 +177,37 @@ def decobrir_bruto():
 
     # Layout
     layout_bt = [
-        [Sg.Text('Esta calculadora serve para tirar a margem de promoção do seu preço.')],
-        [Sg.Text('Preço atual:', size=(20, 1)), Sg.Input('0', key='preco', size=(8, 1))],
-        [Sg.Text('Percentual da promoção:', size=(20, 1)), Sg.Input('0', key='promocao', size=(8, 1))],
-        [Sg.Text('Etapas do cálculo:')],
+        [Sg.Text('This calculator serves to take the promotion margin out of your price.')],
+        [Sg.Text('Current price:', size=(20, 1)), Sg.Input('0', key='preco', size=(8, 1))],
+        [Sg.Text('Promotion percentage:', size=(20, 1)), Sg.Input('0', key='promocao', size=(8, 1))],
+        [Sg.Text('Calculation steps:')],
         [Sg.Output(key='calculo', size=(45, 4))],
-        [Sg.Button('Calcular'), Sg.Button('Voltar', button_color=('black', 'lightgrey'))]
+        [Sg.Button('Run'), Sg.Button('Back', button_color=('black', 'lightgrey'))],
+        [Sg.Text('Created by Victor G. Hermogenes.')]
     ]
 
     # Window
-    window_bt = Sg.Window('Descobrir preço bruto (sem promoção)', layout_bt, location=(350, 1))
+    window_bt = Sg.Window('Discover gross price (no promotion)', layout_bt, location=(350, 1))
 
     # Ler eventos
     while True:
         event_bt, values_bt = window_bt.read()
 
         # Fechar caso necessário
-        if event_bt == 'Voltar' or event_bt == Sg.WIN_CLOSED:
+        if event_bt == 'Back' or event_bt == Sg.WIN_CLOSED:
             break
 
         # Seguir com os cálculos
-        elif event_bt == 'Calcular':
+        elif event_bt == 'Run':
             try:
                 preco = round(float(values_bt['preco'].replace(',', '.')), 2)
                 promocao = round(float(values_bt['promocao'].replace(',', '.').replace('%', '')), 2)/100
-                calculo = f"""etapa1 = {preco}-({preco}*{promocao})
-etapa2 = {preco}-{round(preco*promocao, 2)}
-res = R$ {round(preco-(preco*promocao), 2)}""".replace('.', ',')
+                calculo = f"""step1 = {preco}-({preco}*{promocao})
+                step2 = {preco}-{round(preco*promocao, 2)}
+                result = R$ {round(preco-(preco*promocao), 2)}""".replace('.', ',')
                 window_bt['calculo'].update(calculo)
             except ValueError:
-                Sg.Popup('Impossível calcular, algum dos campos está vazio! Tente novamente', location=(350, 0))
+                Sg.Popup('Impossible to calculate, one of the fields is empty! Try again', location=(350, 0))
                 break
 
     window_bt.close()
@@ -215,33 +219,34 @@ def diferenca_de_preco():
 
     # Layout
     layout_dif = [
-        [Sg.Text('Esta calculadora serve para você descobrir a diferença bruta\n e em percentual do seu concorrente')],
-        [Sg.Text('Menor valor', size=(15, 1)), Sg.Input('0', key='menor', size=(8, 1))],
-        [Sg.Text('Maior valor', size=(15, 1)), Sg.Input('0', key='maior', size=(8, 1))],
-        [Sg.Text('Etapas do cálculo')],
+        [Sg.Text('This calculator is for you to discover the gross difference\n and in percentage of your competitor')],
+        [Sg.Text('Lower value:', size=(15, 1)), Sg.Input('0', key='menor', size=(8, 1))],
+        [Sg.Text('Highest value:', size=(15, 1)), Sg.Input('0', key='maior', size=(8, 1))],
+        [Sg.Text('Calculation steps:')],
         [Sg.Output(key='calculo', size=(45, 3))],
-        [Sg.Button('Calcular'), Sg.Button('Voltar', button_color=('black', 'lightgrey'))]
+        [Sg.Button('Run'), Sg.Button('Back', button_color=('black', 'lightgrey'))],
+        [Sg.Text('Created by Victor G. Hermogenes.')]
     ]
 
     # Window
-    window_dif = Sg.Window('Diferença de valores (entre você e o concorrente)', layout_dif, location=(350, 0))
+    window_dif = Sg.Window('Difference in values (between you and the competitor)', layout_dif, location=(350, 0))
 
     # Ler eventos
     while True:
         event_dif, values_dif = window_dif.read()
 
         # Fechar caso necessário
-        if event_dif == 'Voltar' or event_dif == Sg.WIN_CLOSED:
+        if event_dif == 'Back' or event_dif == Sg.WIN_CLOSED:
             break
-        elif event_dif == 'Calcular':
+        elif event_dif == 'Run':
             try:
                 menor = round(float(values_dif['menor'].replace(',', '.')), 2)
                 maior = round(float(values_dif['maior'].replace(',', '.')), 2)
-                calculo = f"""etapa1 = {maior}-{menor} | ({menor}/{maior}-1)*-1
-resultado = R$ {round(maior-menor, 2)} | {round(((menor/maior-1)*-1)*100,2)}%""".replace('.', ',')
+                calculo = f"""step1 = {maior}-{menor} | ({menor}/{maior}-1)*-1
+                result = R$ {round(maior-menor, 2)} | {round(((menor/maior-1)*-1)*100,2)}%""".replace('.', ',')
                 window_dif['calculo'].update(calculo)
             except ValueError:
-                Sg.Popup('Impossível calcular, algum dos campos está vazio! Tente novamente', location=(350, 0))
+                Sg.Popup('Impossible to calculate, one of the fields is empty! Try again', location=(350, 0))
                 break
 
     window_dif.close()
